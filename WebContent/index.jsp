1e3577d70c4f4ceeff5d9bcfee8dfa5f39072a9b
RuicongSu <rs843@njit.edu>
2016-12-29 15:14:01 -0500

    Initialize

---
+++ WebContent/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="Action" method="get">
+		user:
+		<input name="name" type="text" />
+		pass:
+		<input name="pass" type="password">
+		<input type="submit" value="OK" />
+	</form>
+	<script type="text/javascript">
+		var a = 10;
+		alert(a);
+	</script>
+</body>
+</html>
---
+++ src/servlet/Action.java
@@ -0,0 +1,57 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Action
+ */
+@WebServlet("/Action")
+public class Action extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Action() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#servce(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		System.out.println("Content Length: " + request.getContentLength());
+		System.out.println("Content Type: " + request.getContentType());
+		System.out.println("Content Path: " + request.getContextPath());
+		System.out.println("Local Address: " + request.getLocalAddr());
+		System.out.println("Local Name: " + request.getLocalName());
+		System.out.println("Local Port: " + request.getLocalPort());
+		System.out.println("Locale: " + request.getLocale());
+		System.out.println("Method: " + request.getMethod());
+		System.out.println("Protocol: " + request.getProtocol());
+		System.out.println("Parameter Map: " + request.getParameterMap());
+		System.out.println("Remote Address: " + request.getRemoteAddr());
+		System.out.println("Remote Host: " + request.getRemoteHost());
+		System.out.println("Remote Port: " + request.getRemotePort());
+		System.out.println("Remote User: " + request.getRemoteUser());
+		System.out.println("Session Id: " + request.getRequestedSessionId());
+		System.out.println("URI: " + request.getRequestURI());
+		System.out.println("URL: " + request.getRequestURL());
+		System.out.println("Scheme: " + request.getScheme());
+		System.out.println("Server Name: " + request.getServerName());
+		System.out.println("Server Port: " + request.getServerPort());
+		System.out.println("Servlet Path: " + request.getServletPath());
+		System.out.println("Servlet Context: " + request.getServletContext());
+		System.out.println("Session: " + request.getSession());
+		System.out.println("User Principal: " + request.getUserPrincipal());
+	}
+}
