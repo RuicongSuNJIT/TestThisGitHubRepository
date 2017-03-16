6a74b9847c1ef2038972016337dbdff3f572f9a1
surui <surui@DESKTOP-93HBRO3.resource.campus.njit.edu>
2017-03-15 20:06:07 -0400

    Examples - Upload File Send file using jQuery File Upload Receive file using basic Servlet API founded by Apache Tomcat

--- WebContent/new_comment.jsp
+++ WebContent/new_comment.jsp
@@ -1,11 +1,13 @@
-<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Insert title here</title>
 <%@include file="/common/head.jsp"%>
-<link href="<c:url value="/css/root/new_comment.css"/>" type="text/css" rel="stylesheet" />
+<link href="<c:url value="/css/root/new_comment.css"/>" type="text/css"
+	rel="stylesheet" />
 </head>
 <body>
 	<%@include file="/common/top.jsp"%>
@@ -13,18 +15,31 @@
 		<form>
 			<input id="comment" type="text" />
 			<input id="open" type="button" value="Open" />
-			<input id="file" type="file" accept="image/*" style="display: none" multiple="multiple"/>
+			<input id="file" type="file" accept="image/*" style="display: none"
+				multiple="multiple" onchange="upload()"/>
 		</form>
 	</div>
 </body>
 <%@include file="/common/js_include.jsp"%>
 <script type="text/javascript">
-	$('#open').on('click', function() {
-		$('#file').trigger('click');
+	$(function(){
+		$('#open').on('click', function() {
+			$('#file').trigger('click');
+		});
+
+		$('#file').fileupload({
+			'url' : 'uploadFile', 	// this is the mapped name of the servlet writing into 
+									// the annotation
+			'type' : 'POST',
+			'dataType' : 'json',
+			'done' : function(e, data) {
+				alert(data.result.status);
+			}
+		});
 	});
 
 	function toRegister() {
-		window.location.href = "register.jsp";
+		window.location.href = 'register.jsp';
 	}
 
 	function login() {
@@ -32,8 +47,8 @@
 			'url' : 'login',
 			'type' : 'get',
 			'data' : {
-				'name' : $("#name")[0].value,
-				'pass' : $("#pass")[0].value
+				'name' : $('#name')[0].value,
+				'pass' : $('#pass')[0].value
 			},
 			'dataType' : 'json',
 			'success' : function(returnData) {
---
+++ src/surc/test/UploadFile.java
@@ -0,0 +1,65 @@
+package surc.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class FileUpload
+ */
+// Since I allow the user to upload multiple files at one time, we need to give the servlet
+// a new annotation @MultipartConfig. So, the servlet has two annotations.
+@MultipartConfig
+@WebServlet("/uploadFile")
+public class UploadFile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UploadFile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		JSONObject obj = new JSONObject();
+
+		// Use this segment to get the files.
+		Collection<Part> files = request.getParts();
+
+		// Use for each format to get every file. Instead output the file name, I need you
+		// to save the files in local position. And save the local url into session. Key
+		// is the file name and the value is the local path. Additionally, I think you need
+		// to give the file a new name based on the time stamp. For example, 
+		// 'Image20170315195745' means the file is start writing to local disk at 19:57:45,
+		// on March/15/2017. For the extra name, you need to judge whether it is a jpg, gif
+		// or some other type. I have limited the default kind of file to be image, which
+		// means that when the open file dialog pop up, it will typically should only image
+		// files. Although the user now can change the file filter to all files, we do not 
+		// judge whether the file is an image or not.
+		for(Part file : files) {
+			System.out.println(file.getSubmittedFileName());
+		}
+		
+		obj.put("status", "OK");
+		out.println(obj);
+	}
+
+}
