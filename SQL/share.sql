2bda2181ce589c3e0b74a9d3545c4da890a33636
lsf <lsf921016@126.com>
2017-03-31 19:19:52 -0400

    update sql

--- SQL/share.sql
+++ SQL/share.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: share
 -- ------------------------------------------------------
@@ -24,10 +24,14 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `username` varchar(15) NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
+  `password` varchar(45) NOT NULL,
   `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `nickname` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filePath` varchar(45) DEFAULT NULL,
+  `avatar` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +40,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('1','1','1'),('2','2','2'),('admin','admin',NULL);
+INSERT INTO `users` VALUES ('aa','a','',NULL,1,NULL,NULL),('admin','admin',NULL,NULL,2,NULL,NULL),('b','bb','',NULL,3,NULL,NULL),('lsf','1','1@qq.com',NULL,4,NULL,NULL),('susu','123','susu@qq.com',NULL,5,NULL,NULL),('lsf','19921016','lsf921016@126.com','shuishui',6,NULL,'/dinosaur/resource/avatar.jpg'),('aaaa','aaaa','aaaa','aaaa',9,NULL,'/dinosaur/resource/avatar.jpg'),('qqqq','qqqq','qqqq','qqqq',10,NULL,'/dinosaur/resource/avatar.jpg'),('ccc','ccc','ccc','cccc',11,NULL,'/dinosaur/resource/avatar.jpg');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-01-20 11:57:40
+-- Dump completed on 2017-03-31 19:18:12
