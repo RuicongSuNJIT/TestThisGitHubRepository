b354ab24e68167e59a97d0a7c8395f7de98e119a
surui <surui@DESKTOP-93HBRO3.resource.campus.njit.edu>
2017-03-31 19:40:21 -0400

    Add ftp library

---
+++ src/core/FTP.java
@@ -0,0 +1,36 @@
+package core;
+
+import java.io.InputStream;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+public class FTP {
+	public static void upload(InputStream file, String fileName) {
+		try {
+			JSch jsch = new JSch();
+			Session session = jsch.getSession(Constants.FTP_USER, Constants.FTP_HOST, 22);
+			session.setConfig("PreferredAuthentications", "password");
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.setPassword(Constants.FTP_PASS);
+			session.connect();
+			Channel channel = session.openChannel("sftp");
+			ChannelSftp sftp = (ChannelSftp) channel;
+			sftp.connect();
+			sftp.put(file, Constants.FTP_DIR + fileName);
+			sftp.quit();
+			channel.disconnect();
+			session.disconnect();
+		} catch (JSchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SftpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
