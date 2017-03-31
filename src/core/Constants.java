b4cbf6bc1bd50d103f20afa3e1c178f23e876bc7
surui <surui@DESKTOP-93HBRO3.resource.campus.njit.edu>
2017-03-31 17:33:55 -0400

    Add Constants

---
+++ src/core/Constants.java
@@ -0,0 +1,12 @@
+package core;
+
+public class Constants {
+	public static String FTP_HOST = "afs1.njit.edu";
+	public static String FTP_USER = "rs843";
+	public static String FTP_PASS = "jim1414213";
+	public static String FTP_DIR = "/afs/cad.njit.edu/u/r/s/rs843/public_html/";
+	public static int FTP_PORT = 22;
+	public static String FTP_BASEPATH = "https://web.njit.edu/~rs843/";
+	
+	public static String DEFAULT_AVATAR = FTP_BASEPATH + "Avatar.jpg";
+}
