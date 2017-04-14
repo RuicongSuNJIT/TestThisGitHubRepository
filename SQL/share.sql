0faffdadded4fd9cc7cd38acd306c6a20e2628ab
lsf <lsf921016@126.com>
2017-04-14 13:51:51 -0400

    commit sql

--- SQL/share.sql
+++ SQL/share.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `share` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `share`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: share
@@ -16,6 +18,59 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `text` varchar(200) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `no` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`no`),
+  KEY `id_idx` (`userId`),
+  CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES ('啊啊啊啊啊啊啊',1,9),('啊啊啊啊啊啊啊啊',1,10);
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files` (
+  `filePath` varchar(100) NOT NULL,
+  `commentNo` int(11) NOT NULL,
+  PRIMARY KEY (`filePath`,`commentNo`),
+  KEY `commentNo_idx` (`commentNo`),
+  CONSTRAINT `commentNo` FOREIGN KEY (`commentNo`) REFERENCES `comment` (`no`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `files`
+--
+
+LOCK TABLES `files` WRITE;
+/*!40000 ALTER TABLE `files` DISABLE KEYS */;
+INSERT INTO `files` VALUES ('https://web.njit.edu/~rs843/Images/201709130809321492128572461.jpg',9),('https://web.njit.edu/~rs843/Images/201709130809341492128574295.jpg',9),('https://web.njit.edu/~rs843/Images/201709130809371492128577146.jpg',9),('https://web.njit.edu/~rs843/Images/201710130810351492128635458.jpg',10),('https://web.njit.edu/~rs843/Images/201710130810371492128637671.jpg',10);
+/*!40000 ALTER TABLE `files` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,7 +86,7 @@ CREATE TABLE `users` (
   `filePath` varchar(45) DEFAULT NULL,
   `avatar` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +95,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('aa','a','',NULL,1,NULL,NULL),('admin','admin',NULL,NULL,2,NULL,NULL),('b','bb','',NULL,3,NULL,NULL),('lsf','1','1@qq.com',NULL,4,NULL,NULL),('susu','123','susu@qq.com',NULL,5,NULL,NULL),('lsf','19921016','lsf921016@126.com','shuishui',6,NULL,'/dinosaur/resource/avatar.jpg'),('aaaa','aaaa','aaaa','aaaa',9,NULL,'/dinosaur/resource/avatar.jpg'),('qqqq','qqqq','qqqq','qqqq',10,NULL,'/dinosaur/resource/avatar.jpg'),('ccc','ccc','ccc','cccc',11,NULL,'/dinosaur/resource/avatar.jpg');
+INSERT INTO `users` VALUES ('aa','a','',NULL,1,NULL,NULL),('admin','admin',NULL,NULL,2,NULL,NULL),('b','bb','',NULL,3,NULL,NULL),('lsf','1','1@qq.com',NULL,4,NULL,NULL),('susu','123','susu@qq.com',NULL,5,NULL,NULL),('lsf','19921016','lsf921016@126.com','shuishui',6,NULL,'/dinosaur/resource/avatar.jpg'),('aaaa','aaaa','aaaa','aaaa',9,NULL,'/dinosaur/resource/avatar.jpg'),('qqqq','qqqq','qqqq','qqqq',10,NULL,'/dinosaur/resource/avatar.jpg'),('ccc','ccc','ccc','cccc',11,NULL,'/dinosaur/resource/avatar.jpg'),('lin','1992','lsf921016@126.com','lin',12,NULL,'https://web.njit.edu/~rs843/Avatar.jpg');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-31 19:18:12
+-- Dump completed on 2017-04-14 13:51:07
